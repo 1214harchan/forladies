282a401d7941a9f5e34b71127bd66b37068ac00f
1214harchan <93026473+1214harchan@users.noreply.github.com>
2021-12-08 07:05:19 +0900

    12/8修正

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('forladies.index');
-});
+})->name('/');
 
 Route::get('forladies', 'App\Http\Controllers\ForladiesController@show');
 
