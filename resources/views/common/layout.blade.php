364c5cd06c6975d786b58d45eba5da28c61e3228
1214harchan <93026473+1214harchan@users.noreply.github.com>
2021-12-09 06:53:49 +0900

    12/9修正

--- resources/views/common/layout.blade.php
+++ resources/views/common/layout.blade.php
@@ -25,20 +25,7 @@
       </div>
         @yield('content')
     </div>
-   <footer class="footer" data-background-color="black">
-      <div class=" container ">
-        <nav>
-        </nav>
-        <div class="copyright" id="copyright">
-          &copy;
-          <script>
-            document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
-          </script>, Designed by
-          <a href="https://www.invisionapp.com" target="_blank">Invision</a>. Coded by
-          <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a>.
-        </div>
-      </div>
-    </footer>
+   @yield('footer')
   </div>
   <!--   Core JS Files   -->
   <script src="./assets/js/core/jquery.min.js" type="text/javascript"></script>
