557b3f3bb1b00fb2e3eed442426d8f154a37ab32
1214harchan <93026473+1214harchan@users.noreply.github.com>
2021-12-11 13:01:01 +0900

    UserController更新

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
-    //
+    public function index(Request $request){
+        $items = Users::all();
+        return view('person.index', ['items' => $items]);
+    }
 }
